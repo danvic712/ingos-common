f53f272af6d5c132aa0a37c4c9974a9c6755e60d
Danvic.Wang <danvic.wang@lcfuturecenter.com>
2019-11-16 09:48:05 +0800

    Add user login command

--- sample/aspnetcore/aspnetcore-mediatr-tutorial/03_Application/Sample.Application/Commands/UserLoginCommand.cs
+++ sample/aspnetcore/aspnetcore-mediatr-tutorial/03_Application/Sample.Application/Commands/UserLoginCommand.cs
@@ -8,13 +8,41 @@
 // Description:
 //-----------------------------------------------------------------------
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.Serialization;
 
 namespace Sample.Application.Commands
 {
     public class UserLoginCommand : IRequest<bool>
     {
+        /// <summary>
+        /// 账户
+        /// </summary>
+        [DataMember]
+        public string Account { get; private set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [DataMember]
+        public string Password { get; private set; }
+
+        /// <summary>
+        /// 验证码
+        /// </summary>
+        [DataMember]
+        public string VerificationCode { get; private set; }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="account">账户</param>
+        /// <param name="password">密码</param>
+        /// <param name="verificationCode">验证码</param>
+        public UserLoginCommand(string account, string password, string verificationCode)
+        {
+            Account = account;
+            Password = password;
+            VerificationCode = verificationCode;
+        }
     }
 }
